d78da7b0b054413cde375d6c3a5029e208f202a6
jeganrajrajan <48787752+jeganrajrajan@users.noreply.github.com>
2021-10-26 19:43:10 +0530

    updated the new files

---
+++ input_example.py
@@ -0,0 +1,12 @@
+# Example 1: Python get user input with a message
+name = input('Enter the name: ')
+
+print("Hi "  ,name)
+
+# Example 2: Integer input in Python
+
+num = int(input("Enter a number: "))
+sub = num-3
+print('the sub value is: ', sub)
+
+
---
+++ output.py
@@ -0,0 +1,13 @@
+# How to Display Output in Python
+
+# Example: Python Print Output with default parameter
+
+print("ABCD")
+
+print('A', 'B', 'C', 'D')
+
+# Example: Python Print Output with custom sep&end parameter
+print("ABCD", end="@")
+
+# code for disabling the softspace feature
+print('A', 'B', 'C','D', sep="#")
